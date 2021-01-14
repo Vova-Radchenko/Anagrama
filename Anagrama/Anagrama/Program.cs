2d87061ef3438baf0d5e590bd79899ed41b76ef2
Вова Радченко <vettel070897@gmail.com>
2021-01-14 21:58:24 +0200

    work program

--- Anagrama/Anagrama/Program.cs
+++ Anagrama/Anagrama/Program.cs
@@ -6,7 +6,38 @@ namespace Anagrama
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter string: ");
+            String s = Console.ReadLine();
+            char[] mas = s.ToCharArray();
+            char[] masAnagram = new char[mas.Length];
+            String sAnagram = "";
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (!char.IsLetter(mas[i]))
+                {
+                    masAnagram[i] = mas[i];
+                }
+            }
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (char.IsLetter(mas[i]))
+                {
+                    for (int k = masAnagram.Length - 1; k >= 0; k --)
+                    {
+                        if(masAnagram[k] == '\0')
+                        {
+                            masAnagram[k] = mas[i];
+                            break;
+                        }
+                    }
+                }
+            }
+
+            sAnagram = new string(masAnagram);
+
+            Console.WriteLine("New string is: " + sAnagram);
         }
     }
 }
